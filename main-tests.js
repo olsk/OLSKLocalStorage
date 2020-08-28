943c1ee5e18b657410cbb74168cae9146f1afa95
Rosano Coutinho <pub@xeari.com>
2020-08-28 16:12:08 -0300

    Spec OLKSLocalStorageSet

--- main-tests.js
+++ main-tests.js
@@ -1,1 +1,50 @@
+const { throws, deepEqual } = require('assert');
 
+const mod = require('./main.js');
+
+const uStorage = function () {
+	const _data = {};
+
+	return {
+		setItem (param1, param2) {
+			_data[param1] = param2;
+		},
+		_FakeData () {
+			return _data;
+		},
+	};
+};
+
+describe('OLKSLocalStorageSet', function OLKSLocalStorageSet() {
+
+	it('throws error if param1 not localStorage', function() {
+		throws(function() {
+			mod.OLKSLocalStorageSet({});
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws error if param2 not string', function() {
+		throws(function() {
+			mod.OLKSLocalStorageSet(uStorage(), null, 'bravo');
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws error if param2 not filled', function() {
+		throws(function() {
+			mod.OLKSLocalStorageSet(uStorage(), ' ', 'bravo');
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns param3', function () {
+		deepEqual(mod.OLKSLocalStorageSet(uStorage(), 'alfa', 'bravo'), 'bravo')
+	});
+
+	it('calls setItem', function () {
+		const item = uStorage();
+
+		mod.OLKSLocalStorageSet(item, 'alfa', 'bravo');
+
+		deepEqual(item._FakeData()['alfa'], 'bravo');
+	});
+
+});
--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-
 const mod = {
+
+	OLKSLocalStorageSet (param1, param2, param3) {
+		if (typeof param1 !== 'object' || param1 === null) {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof param1.setItem !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof param2 !== 'string' || !param2.trim()) {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		param1.setItem(param2, param3);
+
+		return param3;
+	},
+
 };
 
 Object.assign(exports, mod);
